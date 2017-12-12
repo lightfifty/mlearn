ed343128fe814bddb46fde06a4d13d41c5e1feb9
git <git@115.159.115.209:/home/git/MyWifi/MyWifi.git>
2017-12-12 23:38:47 +0800

    xadmin 个性化的一些配置

---
+++ apps/organization/adminx.py
@@ -0,0 +1,34 @@
+# _*_ coding: utf-8 _*_
+"""
+     @author: wuxiang           
+     @file:adminx.py          
+     @time:2017/12/12 下午9:58      
+"""
+import xadmin
+
+from .models import CityDict, CourseOrg, Teacher
+
+
+class CityDictAdmin(object):
+    list_display = ['name', 'desc', 'add_time']
+    search_fields = ['name', 'desc']
+    list_filter = ['name', 'desc', 'add_time']
+
+
+class CourseOrgAdmin(object):
+    list_display = ['name', 'desc', 'click_num', 'fav_nums', 'image', 'address', 'city', 'add_time']
+    search_fields = ['name', 'desc', 'click_num', 'fav_nums', 'image', 'address', 'city__name']
+    list_filter = ['name', 'desc', 'click_num', 'fav_nums', 'image', 'address', 'city', 'add_time']
+
+
+class TeacherAdmin(object):
+    list_display = ['org', 'name', 'work_years', 'work_company', 'work_position', 'points', 'click_num', 'fav_nums',
+                    'add_time']
+    search_fields = ['org__name', 'name', 'work_years', 'work_company', 'work_position', 'points', 'click_num', 'fav_nums']
+    list_filter = ['org', 'name', 'work_years', 'work_company', 'work_position', 'points', 'click_num', 'fav_nums',
+                   'add_time']
+
+
+xadmin.site.register(CityDict, CityDictAdmin)
+xadmin.site.register(CourseOrg, CourseOrgAdmin)
+xadmin.site.register(Teacher, TeacherAdmin)
--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -7,12 +7,48 @@
      这个文件是xadmin注册模型专用的
 """
 import xadmin
+from xadmin import views
 
 from users.models import EmailVerifyRecord
+from users.models import Banner
+
+
+# 主题管理类
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSettings(object):
+    site_title = "学堂在线管理系统"
+    site_footer = "学堂在线"
+    menu_style = "accordion"
+
+
+# 注册设置类
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
 
 
 class EmailVerifyRecordAdmin(object):
-    pass
+    # 设置列表显示的字段
+    list_display = ['code', 'email', 'send_type', 'send_time']
+    # 设置可以检索的字段
+    search_fields = ['code', 'email', 'send_type']
+    # 过滤字段
+    list_filter = ['code', 'email', 'send_type', 'send_time']
 
 
-xadmin.site.register(EmailVerifyRecord,EmailVerifyRecordAdmin)
+xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
+
+
+class BannerAdmin(object):
+    # 设置列表显示的字段
+    list_display = ['title', 'image', 'url', 'index', 'add_time']
+    # 设置可以检索的字段
+    search_fields = ['title', 'image', 'url', 'index']
+    # 过滤字段
+    list_filter = ['title', 'image', 'url', 'index', 'add_time']
+
+
+xadmin.site.register(Banner, BannerAdmin)
